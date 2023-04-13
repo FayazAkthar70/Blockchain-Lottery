43a44045fe836389f45720d882c9a61a0a836f10
fayaz <fayazakthar70@gmail.com>
2023-04-13 18:29:02 +0530

    wrote deployment scripts for lottery.sol

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,10 +6,46 @@ require("hardhat-gas-reporter");
 require("hardhat-contract-sizer");
 require("dotenv").config();
 
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
 const rpc_url = process.env.SEPOLIA_RPC_URL;
 console.log(rpc_url);
 console.log();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.7",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      // gasPrice: 130000000000,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111,
+      blockConfirmations: 6,
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+    player: {
+      default: 1,
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    // coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
---
+++ test/unit/Lottery.test.js
@@ -0,0 +1,25 @@
+const { deployments, ethers, getNamedAccounts } = require("hardhat");
+const { assert, expect } = require("chai");
+const { developmentChains } = require("../../helper-hardhat-config");
+
+!developmentChains.includes(network.name)
+  ? describe.skip
+  : describe("FundMe", () => {
+      let fundMe, deployer, VRFCoordinatorV2Mock;
+      beforeEach(async () => {
+        deployer = (await getNamedAccounts()).deployer;
+        await deployments.fixture(["all"]);
+        fundMe = await ethers.getContract("FundMe", deployer);
+        MockV3Aggregator = await ethers.getContract(
+          "MockV3Aggregator",
+          deployer
+        );
+      });
+
+      describe("constructor", () => {
+        it("assigns address correctly", async () => {
+          const response = await fundMe.getPriceFeed();
+          assert.equal(response, MockV3Aggregator.address);
+        });
+      });
+    });
